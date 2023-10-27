b8dfd968ac766dccea6e9e5ceef66b9a2ae1ad2b
Kostya Popov <kepel2823@gmail.com>
2023-10-27 21:17:42 +0300

    Add project files.

---
+++ RetirementCalculation/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetirementCalculation
+{
+    public class Person
+    {
+        public delegate double PrintFunc(int age, int expeareance);
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public int Expeareance { get; set; }
+
+        public Person(string name, int age, int expeareance)
+        {
+            Name = name;
+            Age = age;
+            Expeareance = expeareance;
+        }
+
+        public double GetRetirementValue(PrintFunc func)
+        {
+            double pension = func(Age, Expeareance);
+            return pension;
+        }
+    }
+}
---
+++ RetirementCalculation/Program.cs
@@ -0,0 +1,42 @@
+﻿using static RetirementCalculation.Person;
+
+namespace RetirementCalculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person("Kostya", 60, 40);
+
+            PrintFunc usaCalculator = CalculatePensionUSA;
+            PrintFunc japanCalculator = CalculatePensionJapan;
+            PrintFunc germanyCalculator = CalculatePensionGermany;
+
+            var usaPension = person.GetRetirementValue(usaCalculator);
+            var japanPension = person.GetRetirementValue(japanCalculator);
+            var germanyPension = person.GetRetirementValue(germanyCalculator);
+
+            Console.WriteLine($"{person.Name} receives a pension in the United States in the amount of {usaPension}");
+            Console.WriteLine($"{person.Name} receives a pension in Japan in the amount of {japanPension}");
+            Console.WriteLine($"{person.Name} receives a pension in Germany in the amount of {germanyPension}");
+        }
+
+        static double CalculatePensionUSA(int age, int yearsOfExperience)
+        {
+            double pension = (age * 1000) + (yearsOfExperience * 500);
+            return pension;
+        }
+
+        static double CalculatePensionJapan(int age, int yearsOfExperience)
+        {
+            double pension = (age * 1200) + (yearsOfExperience * 600);
+            return pension;
+        }
+
+        static double CalculatePensionGermany(int age, int yearsOfExperience)
+        {
+            double pension = (age * 800) + (yearsOfExperience * 400);
+            return pension;
+        }
+    }
+}
